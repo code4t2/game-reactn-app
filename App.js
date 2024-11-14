7d1766b1e4a539dd0f73f489b259add56623984f
Titu B G <mail4t2+github@gmail.com>
2024-11-14 04:39:14 +0000

    Barebones app

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <View styles={styles.container}>
-      <Text>Hello, World!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-    alignItems: "center",
-  },
-});
+const styles = StyleSheet.create({});
