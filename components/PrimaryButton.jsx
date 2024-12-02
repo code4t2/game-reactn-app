43e42aeb81a9ecf0f629adec8cacc0b5046ade35
Brinda C M <brinda@auctopustechnologies.com>
2024-12-02 08:35:26 +0530

    Changes to buttons and texts

--- components/PrimaryButton.jsx
+++ components/PrimaryButton.jsx
@@ -1,11 +1,43 @@
-import { Text, View } from "react-native";
+import {Pressable, StyleSheet, Text, View} from "react-native";
 
-function PrimaryButton({ children }) {
+function PrimaryButton({children}) {
+  function pressHandler() {
+    console.log("Pressing");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.outerContainer}>
+      <Pressable
+        style={({pressed}) =>
+          pressed ? [styles.innerContainer, styles.pressed] : styles.innerContainer}
+        onPress={pressHandler}
+        android_ripple={{color: '#72063c'}}>
+        <Text style={styles.text}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  innerContainer: {
+    backgroundColor: '#54052b',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+
+})
--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,10 +1,14 @@
-import { StyleSheet, TextInput, View } from "react-native";
+import {StyleSheet, TextInput, View} from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput}
+                 maxLength={2}
+                 keyboardType={"number-pad"}
+                 autoCapitalize={"none"}
+                 autoCorrect={false}/>
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -18,8 +22,23 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#72063c",
+    backgroundColor: '#72063c',
     borderRadius: 8,
     elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
   },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
 });
